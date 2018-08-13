0c9ca78bd546ed7b599b437780beb8fd1cabc026
Oluwajoba Adegboye <oluwajobaadegboye@hotmail.com>
2018-08-12 23:13:21 -0500

    refactoring

--- src/main/java/edu/mum/cs/inventorymanager/controller/api/ProductRestController.java
+++ src/main/java/edu/mum/cs/inventorymanager/controller/api/ProductRestController.java
@@ -1,13 +1,12 @@
 package edu.mum.cs.inventorymanager.controller.api;
 
-import edu.mum.cs.inventorymanager.model.Product;
+import edu.mum.cs.inventorymanager.model.entity.Product;
 import edu.mum.cs.inventorymanager.service.contract.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.validation.Valid;
 import java.util.List;
 
